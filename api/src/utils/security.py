0c68c1a1a302b64049f23dc344864425a8c4dc91
lcalcaterra <lucacalcaterra95@hotmail.it>
2025-11-06 17:53:40 +0100

    test

--- api/src/utils/security.py
+++ api/src/utils/security.py
@@ -7,7 +7,8 @@ SECRET_KEY = os.getenv("JWT_SECRET", "")
 def verify_user(request: Request):
     token = request.cookies.get("session")
     if not token:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+        raise HTTPException(status_code=403, detail="Missing session cookie (from verify_user)")
+        # raise HTTPException(status_code=401, detail="Unauthorized")
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
         return payload["sub"]
